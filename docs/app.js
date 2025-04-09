cf56ba5344daf7f14db2f165e14bf74f21be60b2
darcyaf <darcygail@163.com>
2025-04-10 01:47:08 +0800

    update git pages

---
+++ docs/app.js
@@ -0,0 +1,60 @@
+let web3;
+let contract;
+let contractInstance;
+let userAccount;
+
+window.addEventListener("load", async () => {
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    await window.ethereum.enable();
+    const accounts = await web3.eth.getAccounts();
+    userAccount = accounts[0];
+  } else {
+    alert("请安装 MetaMask 扩展");
+  }
+});
+
+async function deployContract() {
+  const res = await fetch("../contracts/TrustMeet.json");
+  const data = await res.json();
+  contract = new web3.eth.Contract(data.abi);
+
+  const deposit = document.getElementById("depositAmount").value;
+  const meetTime = document.getElementById("meetTime").value;
+
+  contract
+    .deploy({
+      data: data.bytecode,
+      arguments: [web3.utils.toWei(deposit, "ether"), meetTime],
+    })
+    .send({ from: userAccount })
+    .then((instance) => {
+      contractInstance = instance;
+      alert("部署成功，合约地址: " + instance.options.address);
+    });
+}
+
+async function loadContract() {
+  const res = await fetch("../contracts/TrustMeet.json");
+  const data = await res.json();
+  const address = document.getElementById("contractAddress").value;
+  contractInstance = new web3.eth.Contract(data.abi, address);
+  alert("合约已加载");
+}
+
+async function invite() {
+  const invitee = document.getElementById("invitee").value;
+  await contractInstance.methods.invite(invitee).send({ from: userAccount });
+}
+
+async function join() {
+  const deposit = await contractInstance.methods.depositAmount().call();
+  await contractInstance.methods.join().send({
+    from: userAccount,
+    value: deposit,
+  });
+}
+
+async function confirmArrival() {
+  await contractInstance.methods.confirmArrival().send({ from: userAccount });
+}
